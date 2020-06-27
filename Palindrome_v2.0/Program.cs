ecbb6cd1a86c7a60eed1667cafb7ea60a66728c8
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-26 22:08:40 -0300

    First Commit v2.0

---
+++ Palindrome_v2.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Palindrome_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Developed by Igor Oliveira Fonseca
+
+            Console.Write("What's the sequence of characters (can be a word, number, phrase...): ");
+            string sequenceCharacters = Console.ReadLine().ToUpper().Trim();
+
+            int count = 0;
+            for (int i = 0; i < (sequenceCharacters.Length / 2); i++)
+            {
+                if (sequenceCharacters[i] == sequenceCharacters[sequenceCharacters.Length - (i + 1)])
+                {
+                    count++;
+                }                
+            }
+            if (count == (sequenceCharacters.Length / 2))
+            {
+                Console.WriteLine("It's Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("It's not Palindrome!");
+            }
+
+        }
+    }
+}
